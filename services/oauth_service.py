7c72f5934e6a0519c50937a7dba308f5832cc3e4
Peron Pedro <peronpedro@gmail.com>
2026-07-14 21:38:16 -0300

    Fix OAuthService class

--- services/oauth_service.py
+++ services/oauth_service.py
@@ -1,74 +1,30 @@
-from flask import Flask, request, jsonify
+# ==========================================
+# ML COPY
+# Serviço OAuth
+# ==========================================
 
-app = Flask(__name__)
-
-last_code = None
-
-
-@app.route("/")
-def home():
-
-    return "ML COPY OAuth Server Online"
+import requests
 
 
+class OAuthService:
 
-@app.route("/callback")
-def callback():
-
-    global last_code
-
-    code = request.args.get("code")
+    RENDER_URL = "https://ml-copy.onrender.com"
 
 
-    if not code:
+    def get_code(self):
 
-        return "Nenhum código recebido."
-
-
-    last_code = code
+        url = f"{self.RENDER_URL}/last_code"
 
 
-    print(
-        "Código recebido:",
-        code
-    )
+        response = requests.get(
+            url
+        )
 
 
-    return """
-    <h2>Autorização realizada com sucesso.</h2>
-    <p>Pode fechar esta janela.</p>
-    """
+        response.raise_for_status()
 
 
-
-@app.route("/last_code")
-def last_code_route():
-
-    global last_code
+        data = response.json()
 
 
-    if last_code is None:
-
-        return jsonify({
-            "code": None
-        })
-
-
-    code = last_code
-
-
-    last_code = None
-
-
-    return jsonify({
-        "code": code
-    })
-
-
-
-if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=5000
-    )
+        return data.get("code")
